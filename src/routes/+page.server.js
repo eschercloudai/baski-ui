edeab97ab3bc6577d506794e22c3fd5f49dfd97c
Drew Viles <drew@hudson-viles.uk>
2024-01-17 17:06:08 +0000

    run prettier

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,5 +5,5 @@ export async function load() {
 	const endpoint = env.API_URL + `/api/v1/images`;
 	const response = await axios.get(endpoint);
 
-	return { "data": response.data };
+	return { data: response.data };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
 	import Title from '$lib/Title.svelte';
 	import Card from '$lib/Card.svelte';
 
-    export let data;
-
+	export let data;
 </script>
 
 <Title title="The place for image data!"></Title>
@@ -14,15 +13,15 @@
 
 <div class="flex justify-center my-12">
 	<div class="w-8/12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-			{#each data.data as image}
-				<Card
-					image_id={image.image_id}
-					image_name={image.image_name}
-					build_date={image.build_date}
-					k8s_version={image.k8s_version}
-					scan_status={image.scan_status}
-					test_status={image.test_status}
-				></Card>
-			{/each}
+		{#each data.data as image}
+			<Card
+				image_id={image.image_id}
+				image_name={image.image_name}
+				build_date={image.build_date}
+				k8s_version={image.k8s_version}
+				scan_status={image.scan_status}
+				test_status={image.test_status}
+			></Card>
+		{/each}
 	</div>
 </div>
